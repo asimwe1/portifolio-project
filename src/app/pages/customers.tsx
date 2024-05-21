12da6d0305eb10a7085f4d6047e16e3068605e0a
asimwe1 <landryasimwe@gmail.com>
2024-05-21 16:00:46 +0200

    client sector corrected

--- src/app/pages/customers.tsx
+++ src/app/pages/customers.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 import React from "react";
 import "./services.css";
 import Image from 'next/image'
@@ -9,67 +9,75 @@ import { useState } from 'react';
 const Usercustomers = () => {
   return (
     <div>
-          <div className="-my-[50rem]">
-                <h2 className="text-center my-[60rem] font-medium text-red-400 text-10 ">Testominial</h2>
-                <h3 className="text-center text-[27px] font-semibold -my-[57rem]"> See what our clients say about us! </h3>
-                <ul className="flex my-[60rem] mx-[10rem] gap-10" >
-                <div className="h-[25rem] w-[20rem] bg-white shadow-2xl text-center ">
+      <div className="-my-[50rem]">
+        <h2 className="text-center my-[60rem] font-medium text-red-400 text-10 ">Testominial</h2>
+        <h3 className="text-center text-[27px] font-semibold -my-[57rem]"> See what our clients say about us! </h3>
+        <ul className="flex my-[60rem] mx-[10rem] gap-10" >
 
-                  <Image 
-                  className="w-20 h-20 rounded-[50%] mx-[35%] my-16"
-                  src='/785a1b9c359640da6bc9cfe3670b42ba.jpg'
-                  alt=""
-                  width={500}
-                  height={500}
-                  />
-
-                  <p className="text-[13px] px-4">
-                    Excellent in their work and happy and motivated to work with them and also they offer the best services that i have ever seen in <br/> all of the 
-                    companies that i have worked with different things
-                  </p>
-                  <p className="text-left text-red-600 text-[13px] px-6 pt-8">David Ikene</p>
-                  <p className="text-left text-[10px] px-6 pt-1">Master card foundation</p>
-
-                </div>
-                 <div className="h-[25rem] w-[20rem] bg-white shadow-2xl text-center ">
-
-                 <Image 
-                  className="w-20 h-20 rounded-[50%] mx-[35%] my-16"
-                  src='/785a1b9c359640da6bc9cfe3670b42ba.jpg'
-                  alt=""
-                  width={500}
-                  height={500}
-                  />
-                  <p className="text-[13px] px-4">
-                    Excellent in their work and happy and motivated to work with them and also they offer the best services that i have ever seen in <br/> all of the 
-                    companies that i have worked with different things
-                  </p>
-                  <p className="text-left text-red-600 text-[13px] px-6 pt-8">David Ikene</p>
-                  <p className="text-left text-[10px] px-6 pt-1">Master card foundation</p>
-
-                </div>
-               <div className="h-[25rem] w-[20rem] bg-white shadow-2xl text-center ">
-
-               <Image 
-                  className="w-20 h-20 rounded-[50%] mx-[35%] my-16"
-                  src='/785a1b9c359640da6bc9cfe3670b42ba.jpg'
-                  alt=""
-                  width={500}
-                  height={500}
-                  />
-                  <p className="text-[13px] px-4">
-                    Excellent in their work and happy and motivated to work with them and also they offer the best services that i have ever seen in <br/> all of the 
-                    companies that i have worked with different things
-                  </p>
-                  <p className="text-left text-red-600 text-[13px] px-6 pt-8">David Ikene</p>
-                  <p className="text-left text-[10px] px-6 pt-1">Master card foundation</p>
-
-                </div>
+          <div className=" flex space-x-[5rem] overflow-x-scroll no-scrollbar overflow-x-visible ">
 
 
-                </ul>
+            <div className="h-[25rem] w-[50rem] bg-white shadow-2xl text-center ">
 
-              </div>
+              <Image
+                className="w-[10rem] h-[10rem] rounded-[50%] hover:rounded-[25%] mx-[25%] my-5"
+                src='/785a1b9c359640da6bc9cfe3670b42ba.jpg'
+                alt=""
+                width={500}
+                height={500}
+              />
+
+              <p className="text-[13px] px-4">
+                Excellent in their work and happy and motivated to work with them and also they offer the best services that i have ever seen in <br /> all of the
+                companies that i have worked with different things
+              </p>
+              <p className="text-left text-red-600 text-[13px] px-6 pt-8">David Ikene</p>
+              <p className="text-left text-[10px] px-6 pt-1">Master card foundation</p>
+
+            </div>
+            <div className="h-[25rem] w-[50rem] bg-white shadow-2xl text-center ">
+
+              <Image
+                className="w-20 h-20 rounded-[50%] mx-[35%] my-16"
+                src='/785a1b9c359640da6bc9cfe3670b42ba.jpg'
+                alt=""
+                width={500}
+                height={500}
+              />
+              <p className="text-[13px] px-4">
+                Excellent in their work and happy and motivated to work with them and also they offer the best services that i have ever seen in <br /> all of the
+                companies that i have worked with different things
+              </p>
+              <p className="text-left text-red-600 text-[13px] px-6 pt-8">David Ikene</p>
+              <p className="text-left text-[10px] px-6 pt-1">Master card foundation</p>
+
+            </div>
+            <div className="h-[25rem] w-[50rem] bg-white shadow-2xl text-center ">
+
+              <Image
+                className="w-20 h-20 rounded-[50%] mx-[35%] my-16"
+                src='/785a1b9c359640da6bc9cfe3670b42ba.jpg'
+                alt=""
+                width={500}
+                height={500}
+              />
+              <p className="text-[13px] px-4">
+                Excellent in their work and happy and motivated to work with them and also they offer the best services that i have ever seen in <br /> all of the
+                companies that i have worked with different things
+              </p>
+              <p className="text-left text-red-600 text-[13px] px-6 pt-8">David Ikene</p>
+              <p className="text-left text-[10px] px-6 pt-1">Master card foundation</p>
+
+            </div>
+
+
+
+          </div>
+
+
+        </ul>
+
+      </div>
 
     </div>
   )
